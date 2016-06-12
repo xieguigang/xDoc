005899e4ec40f27e53604ee2c15e2440e27f8684
xieguigang <xie.guigang@gmail.com>
2016-06-12 11:38:57 +0800

    xml doc assembly have a problem

---
+++ xDoc/xDoc/Program.vb
@@ -0,0 +1,14 @@
+﻿Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.SoftwareToolkits
+Imports Microsoft.VisualBasic.SoftwareToolkits.XmlDoc
+
+Module Program
+
+    Public Function Main() As Integer
+        Return GetType(Program).RunCLI(App.CommandLine, executeFile:=AddressOf ExecFile)
+    End Function
+
+    Public Function ExecFile(path As String, args As CommandLine) As Integer
+        Dim assm As XmlDoc.Doc = DocAPI.Load(path)
+    End Function
+End Module
