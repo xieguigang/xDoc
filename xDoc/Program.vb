a712f0b41f7394a2ae10446ac938fe4ddeee1f3f
xieguigang <xie.guigang@gmail.com>
2016-11-19 09:30:50 +0800

    improvments on the url builder

--- xDoc/Program.vb
+++ xDoc/Program.vb
@@ -19,11 +19,13 @@ Module Program
 
     <ExportAPI("/Build",
                Info:="Build sdk document library from xml assembly docs with just one simple command.",
-               Usage:="/Build /in <source.xml/*.xml DIR> [/lib <github/xdoc/hexo, default:=github> /out <outDIR>]")>
+               Usage:="/Build /in <source.xml/*.xml DIR> [/lib <github/xdoc/hexo, default:=github> /func <load> /out <outDIR>]")>
     Public Function Build(args As CommandLine) As Integer
         Dim ps As New ProjectSpace()
         Dim path$ = args("/in")
         Dim mdOutputFolder As String = args.GetValue("/out", path.ParentPath & "/docs/")
+
+        Call "Loading xml assembly documents....".__DEBUG_ECHO
 
         If path.FileExists Then
             Call ps.ImportFromXmlDocFile(path)
@@ -36,8 +38,10 @@ Module Program
         Call $"{path} --> {mdOutputFolder}".__DEBUG_ECHO
 
         Dim type As Libraries = args.GetValue("/lib", Libraries.Github, __ctype:=AddressOf GetLibraryType)
+        Dim jsfunc As String = args.GetValue("/func", "load")
+        Dim url As New URLBuilder(type, jsfunc)
 
-        If ps.ExportMarkdownFiles(mdOutputFolder, [lib]:=type) Then
+        If ps.ExportMarkdownFiles(mdOutputFolder, url) Then
             Call "Document library generates success!".__DEBUG_ECHO
         Else
             Call "Job failure!".PrintException
