a25ad46734db0f87ad7a2c1306bfa04097f22b01
谢桂纲 <xie.guigang@gcmodeller.org>
2018-01-13 23:30:28 +0800

    add tree node model

---
+++ ts/README.md
@@ -0,0 +1,7 @@
+## Fix type script imports bug
+
+```
+npm install @types/node
+```
+
+> https://github.com/Microsoft/TypeScript/issues/9545
---
+++ ts/SearchEngine/search.ts
@@ -0,0 +1,46 @@
+﻿// module parsing the GET query parameters and then search the index.json by using binary tree method.
+
+class node {
+
+    /*
+     * The term or key
+     */ 
+    name: string;
+    /*
+     * The short description about this term
+     */ 
+    description: string;
+    /*
+     * A url link to the target page from this search result will build based on this resources string. 
+     */
+    resource: string;
+
+    // binary tree left & right
+    left: node;
+    right: node;
+
+    constructor(name: string, description: string, left: node, right: node) {
+        this.name = name;
+        this.description = description;
+        this.left = left;
+        this.right = right;
+    }
+
+    /*
+     * Get page url based on the @resource string.
+     */
+    getURL() {
+
+    }
+}
+
+class tree {
+
+    root: node;
+
+    // search this binary tree and then returns the node object, and finally using the returned node object build a result url and show the search result description.
+    search(term: string) {
+
+    }
+}
+
