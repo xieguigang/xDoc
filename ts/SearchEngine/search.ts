dcc9e124f18d2be4b042bc3e9999badec6056726
谢桂纲 <xie.guigang@gcmodeller.org>
2018-01-13 23:38:42 +0800

    add basic framework

--- ts/SearchEngine/search.ts
+++ ts/SearchEngine/search.ts
@@ -38,9 +38,41 @@ class tree {
 
     root: node;
 
+    constructor(tree: node) {
+        this.root = tree;
+    }
+
     // search this binary tree and then returns the node object, and finally using the returned node object build a result url and show the search result description.
     search(term: string) {
 
     }
 }
 
+// Get the query parameters and then reutrns the term key for the search input
+function getTerm() {
+
+    var url_string = window.location.href;
+    var url = new URL(url_string);
+    var key = url.searchParams.get("query");
+
+    return key;
+}
+
+function getSearchResult() {
+
+    var key = getTerm();
+
+    // index is the tree root node.
+    $.get("./index.json", function (index) {
+
+        var searchEngine = new tree(index);
+        var node = searchEngine.search(key);
+        var url = node.getURL();
+        var title = "<h2><a href='${url}'>${key}</a></h2>";
+        var description = "<p>${node.description}</p>";
+
+        $("#search-result").html(title + description);
+
+    });
+
+}
