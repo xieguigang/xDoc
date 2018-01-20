9a77b24947b7843e07a573815b6f02f844ca37d3
谢桂纲 <xie.guigang@gcmodeller.org>
2018-01-20 23:34:54 +0800

    add github commit data parser

---
+++ ts/index_search/github-commit.ts
@@ -0,0 +1,46 @@
+﻿// https://stackoverflow.com/questions/17604071/parse-xml-using-javascript
+
+class author {
+
+    avatar: string;
+    name: string;
+    url: string;
+
+    constructor(name: string, url: string, avatar: string) {
+        this.avatar = avatar;
+        this.name = name;
+        this.url = url;
+    }
+}
+
+class commit {
+
+    id: string;
+    link: string;
+    title: string;
+    updated: Date;
+    author: author;
+
+    constructor(id: string, link: string, title: string, updated: Date, author: author) {
+        this.id = id;
+        this.link = link;
+        this.title = title;
+        this.updated = updated;
+        this.author = author;
+    }
+}
+
+/* By default is the github master branch
+ *
+ * @param git: username/repo
+ */
+function getCommits(git: string, branch: string = "master") {
+    var url = `https://github.com/${git}/commits/${branch}.atom`;
+
+    $.get(url, function (xml) {
+        console.log(xml);
+    });
+}
+
+// test
+getCommits("xieguigang/sciBASIC");
