6a80a8ef90e7aa72eac39f33af96f7218f165e00
谢桂纲 <xie.guigang@gcmodeller.org>
2018-01-20 23:37:59 +0800

    change to es5

---
+++ ts/index_search/github-commit.js
@@ -0,0 +1,33 @@
+// https://stackoverflow.com/questions/17604071/parse-xml-using-javascript
+var author = /** @class */ (function () {
+    function author(name, url, avatar) {
+        this.avatar = avatar;
+        this.name = name;
+        this.url = url;
+    }
+    return author;
+}());
+var commit = /** @class */ (function () {
+    function commit(id, link, title, updated, author) {
+        this.id = id;
+        this.link = link;
+        this.title = title;
+        this.updated = updated;
+        this.author = author;
+    }
+    return commit;
+}());
+/* By default is the github master branch
+ *
+ * @param git: username/repo
+ */
+function getCommits(git, branch) {
+    if (branch === void 0) { branch = "master"; }
+    var url = "https://github.com/" + git + "/commits/" + branch + ".atom";
+    $.get(url, function (xml) {
+        console.log(xml);
+    });
+}
+// test
+getCommits("xieguigang/sciBASIC");
+//# sourceMappingURL=github-commit.js.map
--- ts/index_search/search.js
+++ ts/index_search/search.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 // module parsing the GET query parameters and then search the index.json by using binary tree method.
-const jquery_1 = require("jquery");
-class node {
-    constructor(name, description, left, right) {
+var jquery_1 = require("jquery");
+var node = /** @class */ (function () {
+    function node(name, description, left, right) {
         this.name = name;
         this.description = description;
         this.left = left;
@@ -12,22 +12,23 @@ class node {
     /*
      * Get page url based on the @resource string.
      */
-    getURL() {
+    node.prototype.getURL = function () {
         var url = "/" + this.resource
             .replace("[\\]", "/")
             .replace("\.[mM][dD]", ".html");
         return url;
-    }
-}
+    };
+    return node;
+}());
 // The binary tree search engine.
-class tree {
-    constructor(tree) {
+var tree = /** @class */ (function () {
+    function tree(tree) {
         this.root = tree;
     }
     // search this binary tree and then returns the node object, 
     // and finally using the returned node object build a result 
     // url and show the search result description.
-    search(term) {
+    tree.prototype.search = function (term) {
         var np = this.root;
         var cmp = 0;
         while (np) {
@@ -45,8 +46,9 @@ class tree {
         }
         // found nothing, target term is not exists.
         return null;
-    }
-}
+    };
+    return tree;
+}());
 // Get the query parameters and then reutrns the term key for the search input
 function getTerm() {
     var url_string = window.location.href;
@@ -62,8 +64,8 @@ function getSearchResult() {
         var searchEngine = new tree(index);
         var term = searchEngine.search(key);
         var url = term.getURL();
-        var title = `<h2><a href='${url}'>${key}</a></h2>`;
-        var description = `<p>${term.description}</p>`;
+        var title = "<h2><a href='" + url + "'>" + key + "</a></h2>";
+        var description = "<p>" + term.description + "</p>";
         jquery_1.default("#search-result").html(title + description);
     });
 }
