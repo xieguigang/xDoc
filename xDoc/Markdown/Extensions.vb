df07777fa1fa1e0307255deb0375895ef2ef6098
谢桂纲 <xie.guigang@gcmodeller.org>
2018-01-21 14:04:20 +0800

    split source project

--- xDoc/Markdown/Extensions.vb
+++ xDoc/Markdown/Extensions.vb
@@ -1,7 +1,29 @@
-﻿
-''' <summary>
-''' 在Core模块之中只是一个XML assembly的解析器，对markdown文档的导出功能是被放置在这个项目之中的
-''' </summary>
-Module Extensions
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Language
 
-End Module
+Namespace Markdown
+
+    ''' <summary>
+    ''' 在Core模块之中只是一个XML assembly的解析器，对markdown文档的导出功能是被放置在这个项目之中的
+    ''' </summary>
+    Module Extensions
+
+        Dim libraries As Dictionary(Of String, Libraries) =
+            Enums(Of Libraries) _
+            .ToDictionary(Function(x) x.ToString.ToLower)
+
+        ''' <summary>
+        ''' 类型名称的大小写不敏感
+        ''' </summary>
+        ''' <param name="type"></param>
+        ''' <returns>查找失败的时候默认是返回<see cref="Libraries.Github"/></returns>
+        <Extension>
+        Public Function GetLibraryType(type As Value(Of String)) As Libraries
+            If libraries.ContainsKey(type = LCase(+type)) Then
+                Return libraries(+type)
+            Else
+                Return Markdown.Libraries.Github
+            End If
+        End Function
+    End Module
+End Namespace
--- xDoc/Markdown/NamespaceExports.vb
+++ xDoc/Markdown/NamespaceExports.vb
@@ -1,3 +1,59 @@
-﻿Public Class NamespaceExports
+﻿Imports System.Text
+Imports Microsoft.VisualBasic.ApplicationServices.Development.XmlDoc.Assembly
+Imports Microsoft.VisualBasic.Text
 
-End Class
+Namespace Markdown
+
+    Public Class NamespaceExports : Inherits ProjectNamespace
+
+        ''' <summary>
+        ''' Exports for namespace markdown documents
+        ''' </summary>
+        ''' <param name="folderPath"></param>
+        ''' <param name="pageTemplate"></param>
+        ''' <param name="url"></param>
+        Public Sub ExportMarkdownFile(folderPath As String, pageTemplate As String, url As URLBuilder)
+            Dim typeList As New StringBuilder()
+            Dim projectTypes As New SortedList(Of String, ProjectType)()
+
+            For Each pt As ProjectType In Me.Types
+                projectTypes.Add(pt.Name, pt)
+            Next
+
+            Call typeList.AppendLine("|Type|Summary|")
+            Call typeList.AppendLine("|----|-------|")
+
+            For Each pt As ProjectType In projectTypes.Values
+                Dim lines$() = If(pt.Summary Is Nothing, "", pt.Summary) _
+                    .Trim(ASCII.CR, ASCII.LF) _
+                    .Trim _
+                    .lTokens
+                Dim summary$ = If(lines.IsNullOrEmpty OrElse lines.Length = 1,
+                    lines.FirstOrDefault,
+                    lines.First & " ...")
+                Dim link As String = url.GetNamespaceTypeUrl(Me, pt)
+
+                Call typeList.AppendLine($"|{link}|{summary}|")
+            Next
+
+            Dim text As String
+            Dim path$ = url.GetNamespaceSave(folderPath, Me) ' *.md output path
+
+            If url.[lib] = Libraries.Hexo Then
+                text = $"---
+title: {Me.Path}
+---"
+                text = text & vbCrLf & vbCrLf & typeList.ToString
+            Else
+                text = vbCr & vbLf & "# {0}" & vbCr & vbLf & vbCr & vbLf & "{1}" & vbCr & vbLf
+                text = String.Format(text, Me.Path, typeList.ToString())
+            End If
+
+            If pageTemplate IsNot Nothing Then
+                text = pageTemplate.Replace("[content]", text)
+            End If
+
+            Call text.SaveTo(path, UTF8WithoutBOM)
+        End Sub
+    End Class
+End Namespace
