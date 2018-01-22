4f06c4c09bb3823065479c75a7a854e0e78f4977
谢桂纲 <xie.guigang@gcmodeller.org>
2018-01-22 21:10:54 +0800

    add test for overloads

--- test/test1/Class1.vb
+++ test/test1/Class1.vb
@@ -8,4 +8,37 @@ Public Class Class1
     ''' </summary>
     ''' <returns></returns>
     Public Property M1 As String
+
+    ''' <summary>
+    ''' 11111111111111
+    ''' </summary>
+    Public Overloads Sub overloadsMethod()
+
+    End Sub
+
+    ''' <summary>
+    ''' 2222222
+    ''' </summary>
+    ''' <param name="m2%">AAAA@@@@@@22222</param>
+    Public Overloads Sub overloadsMethod(m2%)
+
+    End Sub
+
+    ''' <summary>
+    ''' 333333
+    ''' </summary>
+    ''' <param name="ss$">AAAAA</param>
+    Public Overloads Sub overloadsMethod(ss$)
+
+    End Sub
+
+    ''' <summary>
+    ''' FFFFFFFF
+    ''' </summary>
+    ''' <param name="a">111111111111</param>
+    ''' <param name="b$">222222222222</param>
+    ''' <returns>lllllllllllllllllllll</returns>
+    Public Overloads Function overloadsMethod(a As Date, b$) As String
+
+    End Function
 End Class
