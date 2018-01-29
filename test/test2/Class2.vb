a03c35f46bb2de0ad73b1f79304270ad279f320c
xieguigang <xie.guigang@gcmodeller.org>
2018-01-29 19:51:33 +0800

    add generic type test

--- test/test2/Class2.vb
+++ test/test2/Class2.vb
@@ -5,7 +5,7 @@ Imports Microsoft.VisualBasic.Language
 ''' <summary>
 ''' FFFFFFFFFFFF
 ''' </summary>
-Public Class Class2
+Public Class Class2(Of T)
 
     ''' <summary>
     ''' WWWWWWWWWWW
@@ -35,14 +35,33 @@ Public Class Class2
     End Sub
 
     ''' <summary>
+    ''' This is a generic method
+    ''' </summary>
+    ''' <typeparam name="V">generic</typeparam>
+    ''' <param name="a">type is <typeparamref name="V"/></param>
+    ''' <returns></returns>
+    Public Function generic(Of V)(a As V) As T
+
+    End Function
+
+    ''' <summary>
     ''' uuuuuuuuuuuuuuuuuuuuuuuuuuuu
     ''' </summary>
     ''' <param name="a"></param>
     ''' <param name="b"></param>
     ''' <returns></returns>
-    Public Shared Operator *(a As Class2, b As Class1) As SaveOptions
+    Public Shared Operator *(a As Class2(Of T), b As Class1) As SaveOptions
 
     End Operator
+End Class
+
+''' <summary>
+''' test
+''' </summary>
+''' <typeparam name="T">type 1</typeparam>
+''' <typeparam name="V">type 2</typeparam>
+Public Class Pair(Of T, V)
+
 End Class
 
 ''' <summary>
