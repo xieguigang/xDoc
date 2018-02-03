1069e4f25301c903dce53e85f0d4c931880f168f
xieguigang <xie.guigang@gcmodeller.org>
2018-02-03 15:45:45 +0800

    build html page

--- xDoc/Markdown/NamespaceExports.vb
+++ xDoc/Markdown/NamespaceExports.vb
@@ -34,7 +34,34 @@ Namespace Markdown
         End Function
 
         Public Function HtmlPage(url As URLBuilder) As String Implements IPageBuilder.HtmlPage
-            Throw New NotImplementedException()
+            Dim typeList As New StringBuilder()
+
+            typeList.AppendLine("<table>")
+            typeList.AppendLine(<thead>
+                                    <tr>
+                                        <th>Type</th>
+                                        <th>Summary</th>
+                                    </tr>
+                                </thead>)
+
+            Call typeList.AppendLine("<tbody>")
+
+            For Each pt As ProjectType In Types.OrderBy(Function(t) t.Name)
+                Dim summary$ = pt.Summary.lTokens.JoinBy("<br />")
+                Dim link As String = url.GetNamespaceTypeUrl(Me, pt)
+
+                Call typeList.AppendLine(
+                    <tr>
+                        <td><a href=<%= link %>><%= pt.Name %></a></td>
+                        <td><%= summary %></td>
+                    </tr>)
+            Next
+
+            Call typeList.AppendLine("</tbody>")
+            Call typeList.AppendLine("</table>")
+
+            Dim text$ = typeList.ToString.MarkdownPage(title:=Me.Path, url:=url)
+            Return text
         End Function
     End Class
 End Namespace
