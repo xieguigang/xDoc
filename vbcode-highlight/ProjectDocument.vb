de6ac8f31ddbcef767fd7b91ac38aa9b95587846
xieguigang <xie.guigang@gcmodeller.org>
2018-02-15 00:56:39 +0800

    to html

--- vbcode-highlight/ProjectDocument.vb
+++ vbcode-highlight/ProjectDocument.vb
@@ -6,13 +6,15 @@ Public Module ProjectDocument
 
     Public Function Build(vbproj$, EXPORT$, Optional schema As Schema = Nothing) As Boolean
         Dim assmInfo As AssemblyInfo = vbproj.GetAssemblyInfo
+        Dim folder$ = vbproj.ParentPath
 
         ' index.html
         Call assmInfo.Summary.SaveTo($"{EXPORT}/index.html")
 
         ' itemgroups\compiles
         For Each file As String In vbproj.EnumerateSourceFiles
-
+            Dim html$ = $"{folder}/{file}".ReadAllText.Render(schema)
+            Call html.SaveTo($"{EXPORT}/{file}".ChangeSuffix("html"))
         Next
 
         Return True
