3e5e5828c23214b312aab169625b1c5f3c8412ce
xieguigang <xie.guigang@gcmodeller.org>
2020-01-29 20:43:01 +0800

    R# code parser module

--- ts/Rscript/Highlights/Syntax.ts
+++ ts/Rscript/Highlights/Syntax.ts
@@ -1,4 +1,28 @@
 export class Syntax {
 
+    private code: CodeStyler = new CodeStyler();
+    private escapes = {
+        string: false,
+        comment: false,
+        keyword: false,
+        quot: "'"
+    }
+    private tokenBuffer: string[] = [];
 
+    public constructor(private chars: Pointer<string>) {
+    }
+
+    public getTokens(): CodeStyler {
+        while (!this.chars.EndRead) {
+            this.walkChar(this.chars.Next);
+        }
+
+
+
+        return this.code;
+    }
+
+    private walkChar(c: string) {
+
+    }
 }
