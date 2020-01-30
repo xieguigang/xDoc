f4c60e7756dd5006154d1b59b5d11c748f1e96b7
xieguigang <xie.guigang@gcmodeller.org>
2020-01-31 02:30:56 +0800

    call function in R#?

--- ts/Rscript/app.ts
+++ ts/Rscript/app.ts
@@ -3,3 +3,10 @@
 $ts.mode = Modes.debug;
 
 
+const hello_world: string = `
+let hello_word as function(word = ["world", 'R#', "GCModeller Users"]) {
+    print(\`Hello \${word}!\`);
+}
+
+call hello_word();
+`;
