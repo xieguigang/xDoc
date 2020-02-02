88fe9c2da7f69925d755d40cf4bd401539ae3757
xieguigang <xie.guigang@gcmodeller.org>
2020-02-02 21:50:42 +0800

    css style model

--- ts/vbcode/CSS.ts
+++ ts/vbcode/CSS.ts
@@ -1,20 +1,5 @@
 ﻿namespace vscode {
 
-    export interface CSS {
-        string: string;
-        comment: string;
-        keyword: string;
-        attribute: string;
-        /**
-         * 用户类型的颜色样式值
-        */
-        type: string;
-        directive: string;
-
-        globalFont: CanvasHelper.CSSFont;
-        lineHeight: string;
-    }
-
     export function defaultStyle(): CSS {
         return <CSS>{
             string: "#a31515",
---
+++ ts/vscode/Styles/css.ts
@@ -0,0 +1,17 @@
+﻿namespace vscode {
+
+    export interface CSS {
+        string: string;
+        comment: string;
+        keyword: string;
+        attribute: string;
+        /**
+         * 用户类型的颜色样式值
+        */
+        type: string;
+        directive: string;
+
+        globalFont: CanvasHelper.CSSFont;
+        lineHeight: string;
+    }
+}
