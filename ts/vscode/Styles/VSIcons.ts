018a5edc718e70bab7e0d8d593bd25c703448c33
xieguigang <xie.guigang@gcmodeller.org>
2020-08-15 21:44:35 +0800

    format icon png base64

--- ts/vscode/Styles/VSIcons.ts
+++ ts/vscode/Styles/VSIcons.ts
@@ -37,30 +37,44 @@ namespace vscode.TOC.View.Icons {
         62zrlhnvVKp7K3zLyx/3SSKVVkEAAAAAElFTkSuQmCC
     `);
 
-    export const vbinterface: string = uri(`data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAA
-                                            7DAAAOwwHHb6hkAAAAkElEQVR4Xu1SIQ6EMBDsT+6ewh/4wLkz/ADDOwiWIPsIFBaD5uyJgmgrC9OEzQ
-                                            ICsRI2mXQ7O7PdtFU3CO/92zmnVwQGDZ6LEmttASA/mE1WtkGlFQF78KhD9Bl+/5DXXQRycGiAk3bmcx
-                                            MNkXl9GyogZ6MS3/ZjmMyMlbhY30QXgJnAG4gnEN+B/BXk/+CJBc8mVhfGFRsSAAAAAElFTkSuQmCC`);
+    export const vbinterface: string = uri(`
+        iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXM
+        AAA7DAAAOwwHHb6hkAAAAkElEQVR4Xu1SIQ6EMBDsT+6ewh/4wLkz/A
+        DDOwiWIPsIFBaD5uyJgmgrC9OEzQICsRI2mXQ7O7PdtFU3CO/92zmnV
+        wQGDZ6LEmttASA/mE1WtkGlFQF78KhD9Bl+/5DXXQRycGiAk3bmcxMN
+        kXl9GyogZ6MS3/ZjmMyMlbhY30QXgJnAG4gnEN+B/BXk/+CJBc8mVhf
+        GFRsSAAAAAElFTkSuQmCC
+    `);
 
-    export const vbmodule: string = uri(`data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAA
-                                         7EAAAOxAGVKw4bAAAAVUlEQVR4Xu2SMQrAMAhFNbh4ot4oa86StTfKiRwtHUoCDkIkBELfIMiXj/hFVY
-                                         UI9BYRmXJhZqSvKddtBmrLrpYgwCEGNB7F0LW1G9ioLBtj/P/A5wEG9hVnmCDA8wAAAABJRU5ErkJggg
-                                         ==`);
+    export const vbmodule: string = uri(`
+        iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXM
+        AAA7EAAAOxAGVKw4bAAAAVUlEQVR4Xu2SMQrAMAhFNbh4ot4oa86StT
+        fKiRwtHUoCDkIkBELfIMiXj/hFVYUI9BYRmXJhZqSvKddtBmrLrpYgw
+        CEGNB7F0LW1G9ioLBtj/P/A5wEG9hVnmCDA8wAAAABJRU5ErkJggg==
+    `);
 
-    export const vbmethod: string = uri(`data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAA
-                                         7EAAAOxAGVKw4bAAACFUlEQVR4Xp2Tz4sScRjGH2UPoWFBC97qHrg3s5Mrmkx509us5OxJ2iYz135snb
-                                         ZNKlwT1iIQvJQdFJJAFLwpG3jph+sG/QHCejDJUdODYN/emcMwDkbQA88ww/B5Zr4vzwvGGPSaTqdnyb
-                                         vj8Vjq9Xq/6f4N+QJ0ktkVHXgaQHw+n8fq9fqZYrGIwWCAQCAQIvMU8hpAwmQy9bUpCkjeoS/2y+UyCw
-                                         aDTBAEVq1W2edPX1gsFmN+v58VCgU2Go3G8t/JjMwqAZPJpF+r1RbAwU+JSYOh6sPDjywcDjOe51mlUm
-                                         HE/FCPYDAYziWTSZjNZty/9wA2mw16rdnW8GQvgcd7u0in03C73asgGaERz28g9WIfz54/Rbd7Aq2Ojl
-                                         qIbd8BzUeLLA7Rd82Hdec6SqX3iN+Nw+PxwOv10nMJ7XYbm8Im7PZLCAnX9QGqlGOEQgI47iry7/KgAV
-                                         LQFbzMvILFYpHnBa2M+IusViu2bmxBlnhTVOBlMuL/tDzg+Nvxv4DlR2CMnUQiEaRS+zjIHGA4HC6FW6
-                                         0WHj7agcvlArV1BkDtwUWO4xIOh0PM5XLGyO1bEGiQDsdlBZzNZjTQt2g0GhBFEU6n8zuADbXKml2wk7
-                                         82m025lUrzqDBKO6PRKOt0OozeZ8inVJYu+k1cIW9LkvQrm80yn8/H8vm8XN0ugRw00gfog86TyxTECP
-                                         xAXl22zn8AcMBI8DVOMowAAAAASUVORK5CYII=`);
+    export const vbmethod: string = uri(`
+        iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXM
+        AAA7EAAAOxAGVKw4bAAACFUlEQVR4Xp2Tz4sScRjGH2UPoWFBC97qHr
+        g3s5Mrmkx509us5OxJ2iYz135snbZNKlwT1iIQvJQdFJJAFLwpG3jph
+        +sG/QHCejDJUdODYN/emcMwDkbQA88ww/B5Zr4vzwvGGPSaTqdnybvj
+        8Vjq9Xq/6f4N+QJ0ktkVHXgaQHw+n8fq9fqZYrGIwWCAQCAQIvMU8hp
+        AwmQy9bUpCkjeoS/2y+UyCwaDTBAEVq1W2edPX1gsFmN+v58VCgU2Go
+        3G8t/JjMwqAZPJpF+r1RbAwU+JSYOh6sPDjywcDjOe51mlUmHE/FCPY
+        DAYziWTSZjNZty/9wA2mw16rdnW8GQvgcd7u0in03C73asgGaERz28g
+        9WIfz54/Rbd7Aq2OjlqIbd8BzUeLLA7Rd82Hdec6SqX3iN+Nw+PxwOv
+        10nMJ7XYbm8Im7PZLCAnX9QGqlGOEQgI47iry7/KgAVLQFbzMvILFYp
+        HnBa2M+IusViu2bmxBlnhTVOBlMuL/tDzg+Nvxv4DlR2CMnUQiEaRS+
+        zjIHGA4HC6FW60WHj7agcvlArV1BkDtwUWO4xIOh0PM5XLGyO1bEGiQ
+        DsdlBZzNZjTQt2g0GhBFEU6n8zuADbXKml2wk782m025lUrzqDBKO6P
+        RKOt0OozeZ8inVJYu+k1cIW9LkvQrm80yn8/H8vm8XN0ugRw00gfog8
+        6TyxTECPxAXl22zn8AcMBI8DVOMowAAAAASUVORK5CYII=
+    `);
 
-    export const vbfield: string = uri(`data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAA
-                                        7EAAAOxAGVKw4bAAABPUlEQVR4XrWSsUvDQBjFv5OCcIKDCuogCM4NAeni5hAcFR0Ewb9Ap6KdBCsoKI
+    export const vbfield: string = uri(`
+        iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXM
+        AAA7EAAAOxAGVKw4bAAABPUlEQVR4XrWSsUvDQBjFv5OCcIKDCuogCM
+        4NAeni5hAcFR0Ewb9Ap6KdBCsoKI
                                         g4OLoVBwctQZ06qYtLLSSb0EEU6iC2Be/W83vBQuBiDIgPHnzf++79skTQD9Ja4zZvDG1jF4J2ePellI
                                         agtCJ7QSndOL9/Mk7xIjJmZLgBnqnola9N2GzBmBNBqcVOuxu3BeJeI0dEj1f1V3evGtLoYD+drc3QxJ
                                         CkNAXPbVo+viNdWXEBcLFAXn4ssfDyoalYqVMtfLNuufiCB/nSTQQ6Wp1GZBVTADYoi/pSbv8HKEwNk7
